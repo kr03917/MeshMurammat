995d87b1cae85183d5199d9ed69cda67259f16df
ZainabAV <31242850+ZainabAV@users.noreply.github.com>
2021-03-31 10:28:47 +0500

    Delete main.cpp

--- Front-end/main.cpp
+++
@@ -1,9 +0,0 @@
-/* cppsrc/main.cpp */
-#include <napi.h>
-#include "./cppsrc/test.h"
-
-Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
-  return test::Init(env, exports);
-}
-
-NODE_API_MODULE(testaddon, InitAll)
