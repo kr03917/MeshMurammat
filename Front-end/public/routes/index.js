a37d1f69a3b9f4be9356b2e987786dfd87535e29
ZainabAV <31242850+ZainabAV@users.noreply.github.com>
2021-03-31 10:32:25 +0500

    Add files via upload

--- Front-end/public/routes/index.js
+++ Front-end/public/routes/index.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var router = express.Router();
 const path = require('path');
-const testAddon = require('./../../build/Release/testaddon.node');
+var multer = require('multer');
+
+// SET STORAGE
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+var upload = multer({ storage: storage })
         
 
 /* GET welcome page. */
@@ -10,16 +21,22 @@ router.get('/meshmurammat', function(req, res, next) {
   
 });
 
+router.get('/PLYExporter', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '/PLYExporter.js'));
+  
+});
+
 /* GET home page. */
 
 router.get('/home', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '/home.html'));
+  res.sendFile(path.join(__dirname, '/../../home.html'));
   
   var exec = require('child_process').execFile;
 
   var fun =function(){
-   console.log("fun() start");
+   //console.log("fun() start");
    exec(path.join(__dirname, '/mesh.exe'), function(err, data) {  
+        
         console.log(err)
         console.log(data.toString());                       
     });  
@@ -27,16 +44,27 @@ router.get('/home', function(req, res, next) {
 fun();
 });
 
-router.get('/cpp', function(req, res, next) {  
-  res.send(testAddon.add(+req.query.a,+req.query.b).toString());
-});
+router.post('/uploadfile', upload.single('mesh'), (req, res, next) => {
+  console.log("file upload post req sent")
+  const file = req.file
+  console.log(file)
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+  res.status(204).send(file)
+  var exec = require('child_process').execFile;
 
-router.get('/meshviewer', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '/meshViewer.html'));
+  var fun =function(){
+   //console.log("fun() start");
+   exec(path.join(__dirname, '/my_executable.exe'), function(err, data) {  
+        
+        console.log(err)
+        console.log(data.toString());                       
+    });  
+}
+fun();
 });
-
-//router.get('/fileupload', function(req, res, next) {  
-//  res.sendFile(path.join(__dirname, '/meshViewer.js'));
-//});
 
 module.exports = router;
